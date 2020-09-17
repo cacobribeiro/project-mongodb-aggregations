4ef3f4c6927ffb042f6229384c7bc756df6e7074
Caio Ribeiro <cacoribeiro@gmail.com>
2020-09-17 19:14:12 -0300

    Desafio 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,22 @@
+db.trips.aggregate([
+  {
+    $match: {
+      birthYear: { $exists: 1 },
+      birthYear: { $nin: [""] },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      maiorAnoNascimento: { $max: { $toInt: "$birthYear" } },
+      menorAnoNascimento: { $min: { $toInt: "$birthYear" } },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      maiorAnoNascimento: 1,
+      menorAnoNascimento: 1,
+    },
+  },
+]);
