0ae9c526762b41cd112825000c08eb0518b43a66
Caio Ribeiro <cacoribeiro@gmail.com>
2020-09-18 13:54:06 -0300

    Desafio 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,26 @@
+db.trips.aggregate([
+  {
+    $match: {
+      startTime: {
+        $gte: ISODate("2016-03-10"),
+        $lt: ISODate("2016-03-11"),
+      },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      duracaoMedia: {
+        $avg: {
+          $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60 * 1000],
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      duracaoMediaEmMinutos: { $round: ["$duracaoMedia"] },
+    },
+  },
+]);
