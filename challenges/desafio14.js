cd5f20e089f0796749d18c59f756b856f0165740
Caio Ribeiro <cacoribeiro@gmail.com>
2020-09-18 14:03:04 -0300

    Desafio 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,34 @@
+db.trips.aggregate([
+  {
+    $match: {
+      startTime: { $exists: 1 },
+      stopTime: { $exists: 1 },
+
+    },
+  },
+  {
+    $group: {
+      _id: "$bikeid",
+      duracaoMedia: {
+        $avg: {
+          $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60 * 1000],
+        },
+      },
+    },
+  },
+  {
+    $sort: {
+      duracaoMedia: -1,
+    },
+  },
+  {
+    $limit: 5,
+  },
+  {
+    $project: {
+      _id: 0,
+      bikeid: "$_id",
+      duracaoMedia: { $ceil: ["$duracaoMedia"] },
+    },
+  },
+]);
